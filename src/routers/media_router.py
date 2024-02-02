d94614c6c43ddaed2f4fa524ddb4cd3253c5ecae
austinibele <austin.ibele@kungfu.ai>
2024-02-02 18:33:16 -0500

    Uploading and Retrieving images now works

--- src/routers/media_router.py
+++ src/routers/media_router.py
@@ -1,15 +1,31 @@
-from fastapi import APIRouter, HTTPException, UploadFile, File
+from fastapi import APIRouter, HTTPException, UploadFile, File, Response
+from fastapi.responses import JSONResponse
 from src.services.media_service import MediaService
 
 router = APIRouter()
 media_service = MediaService()
 
-@router.post("/images/upload", status_code=201, operation_id="upload_image")
-async def upload_image(image_bytes: bytes = File(...)):
+@router.post("/images/upload", status_code=201)
+async def upload_image(image: UploadFile = File(...)):
     try:
+        # Read the image as bytes
+        image_bytes = await image.read()
+
         # Upload the image and get the path
         image_path = media_service.upload_image(image_bytes)
-        return {"success": True, "message": "Image uploaded successfully.", "data": image_path}
+        image_path = "http://localhost:8080/api/media/images/" + image_path.split("/")[-1]
+        
+
+        # Return the URL or path of the uploaded image
+        return JSONResponse(
+            status_code=200,
+            content={
+                "success": 1,
+                "file": {
+                    "url": image_path
+                }
+            }
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -18,7 +34,15 @@ async def get_image(filename: str):
     try:
         # Retrieve the image bytes
         image_bytes = media_service.get_image(filename)
-        return {"success": True, "data": image_bytes}
+        if filename.endswith(".webp"):
+            media_type = "image/webp"
+        elif filename.endswith(".png"):
+            media_type = "image/png"
+        elif filename.endswith(".jpg") or filename.endswith(".jpeg"):
+            media_type = "image/jpeg"
+        else:
+            raise HTTPException(status_code=400, detail="Unsupported image format.")
+        return Response(content=image_bytes, media_type=media_type)
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Image not found.")
     except Exception as e:
